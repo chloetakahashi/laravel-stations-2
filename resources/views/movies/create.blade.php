59d5386ec792481ad0ffd95f9723623988f29211
chloetakahashi <chloe.socrates.7z@gmail.com>
2024-05-06 17:55:07 +0900

    add:create movies form

---
+++ resources/views/movies/create.blade.php
@@ -0,0 +1,31 @@
+<h2>映画を登録する</h2>
+<form action={{ route('idea.store') }} method="post">
+    @csrf
+    <div>映画タイトル</div>
+    <input type="text" id="title" name="title">
+    @error('title')
+        <span>{{ $message }}</span>
+    @enderror
+    <div>画像URL</div>
+    <input type="text" id="image_url" name="image_url">
+    @error('image_url')
+        <span>{{ $message }}</span>
+    @enderror
+    <div>公開年</div>
+    <input type="text" id="published_year" name="published_year">
+    @error('published_year')
+        <span>{{ $message }}</span>
+    @enderror
+    <div>公開中かどうか</div>
+    <input type="checkbox" name="is_showing" id="is_showing" value="0">上映予定
+    <input type="checkbox" name="is_showing" id="is_showing" value="1">上映中
+    @error('is_showing')
+        <span>{{ $message }}</span>
+    @enderror
+    <div>概要</div>
+    <textarea name="description" id="description" cols="30" rows="10"></textarea>
+    @error('description')
+        <span>{{ $message }}</span>
+    @enderror
+    <button type="submit">登録</button>
+</form>
