261a37d7c6474064e58b1fcce5b7a0c423615d82
chloetakahashi <chloe.socrates.7z@gmail.com>
2024-05-06 14:20:15 +0900

    add:add table views for movies

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -1,16 +1,35 @@
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Practices</title>
 </head>
+
 <body>
-    <ul>
+    <table>
+        <tr>
+            <th>映画タイトル</th>
+            <th>画像URL</th>
+            <th>公開年</th>
+            <th>上映中かどうか</th>
+            <th>概要</th>
+            <th>登録日時</th>
+            <th>更新日時</th>
+        </tr>
         @foreach ($movies as $movie)
-            <li>タイトル：{{ $movie->title }}</li>
-            <li><img src={{ $movie->image_url }} alt=""></li>
+            <tr>
+                <td>{{ $movie->title }}</td>
+                <td><img src={{ $movie->image_url }} alt="" width="300" height="200"></td>
+                <td>{{ $movie->published_year }}</td>
+                <td>{{ $movie->is_showing ? '上映中' : '上映予定' }}</td>
+                <td>{{ $movie->description }}</td>
+                <td>{{ $movie->created_at }}</td>
+                <td>{{ $movie->updated_at }}</td>
+            </tr>
         @endforeach
-    </ul>
+    </table>
 </body>
+
 </html>
