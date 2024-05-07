01c9a056e7ead4c9cb835c6386e7e5151154416a
chloetakahashi <chloe.socrates.7z@gmail.com>
2024-05-08 00:04:50 +0900

    add:add edit and update actions for movies

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -38,4 +38,31 @@ class MoviesController extends Controller
 
         return redirect('/admin/movies')->with('success', '映画の登録に成功しました。');
     }
+
+    public function edit($id)
+    {
+        $movie = Movie::findOrFail($id);
+        return view('movies.edit', compact('movie'));
+        // compact('movie') => ['movie' => $movie]
+    }
+
+    public function update(Request $request, $id)
+    {
+        $movie = Movie::findOrFail($id);
+        request()->validate([
+            'title' => 'required|unique:movies,title',
+            'image_url' => 'url',
+            'published_year' => 'integer',
+            'is_showing' => 'required',
+            'description' => 'required',
+        ]);
+        $movie->title = $request->input('title');
+        $movie->image_url = $request->input('image_url');
+        $movie->published_year = $request->input('published_year');
+        $movie->is_showing = $request->input('is_showing');
+        $movie->description = $request->input('description');
+        $movie->save();
+
+        return redirect('/admin/movies')->with('success', '映画の編集に成功しました。');
+    }
 }
