380c6c40435d7854707300cc73e163a10937272b
chloetakahashi <chloe.socrates.7z@gmail.com>
2024-05-19 23:37:09 +0900

    add:add genre_id to movies seed file

--- database/factories/MovieFactory.php
+++ database/factories/MovieFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Genre;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class MovieFactory extends Factory
@@ -17,6 +18,7 @@ class MovieFactory extends Factory
             'title' => $this->faker->unique()->word,
             'image_url' => $this->faker->imageUrl(),
             'published_year' => $this->faker->year,
+            'genre_id' => Genre::insertGetId(['name' => 'ジャンル']),
             'description' => $this->faker->realText(20),
             'is_showing' => $this->faker->boolean,
         ];
